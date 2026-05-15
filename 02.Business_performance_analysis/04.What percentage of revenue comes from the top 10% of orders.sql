207a7ffbe1141cfd4013a8f5ec3684a9563d7842
boddumaheshkumar <boddumaheshkumar@users.noreply.github.com>
2026-05-15 15:00:25 +0530

    Refactor SQL query for calculating revenue share from top 10% of orders for improved readability and structure

--- 02.Business_performance_analysis/04.What percentage of revenue comes from the top 10% of orders.sql
+++ 02.Business_performance_analysis/04.What percentage of revenue comes from the top 10% of orders.sql
@@ -1,25 +1,28 @@
 ---Question 4
----What percentage of revenue comes from the top 10% of orders?
+---What percentage of revenue comes from the top 10% of orders?  
 
-with orders_revenues as (SELECT
-    order_id,
-    sum(order_items.price) as rev
-from order_items
-GROUP BY
-order_id
+WITH orders_revenues AS (
+    SELECT
+        order_id,
+        SUM(order_items.price) AS rev
+    FROM order_items
+    GROUP BY order_id
 ),
 
-ranked_orders as 
-(select
-    order_id,
-    rev, 
-    ntile(10) over(order by rev desc) as revenue_rank
-from
-    orders_revenues)
+ranked_orders AS (
+    SELECT
+        order_id,
+        rev, 
+        ntile(10) OVER (ORDER BY rev DESC) AS revenue_rank
+    FROM orders_revenues
+)
 
-
-select 
-    round(sum(case when revenue_rank =1 then rev end)/ sum(rev)::numeric * 100,2)
-    ||'%' as top_orders_revenue
-from ranked_orders    
-
+SELECT 
+    CASE 
+        WHEN revenue_rank = 1 THEN 'Top 10% Orders'
+        ELSE 'Remaining 90% Orders'
+    END AS order_segment,
+    ROUND(SUM(rev) / SUM(SUM(rev)) OVER () * 100, 2) AS revenue_share
+FROM ranked_orders
+GROUP BY order_segment
+ORDER BY revenue_share DESC
